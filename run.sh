4a289f99b0cf05c2011f97a7c38bdabf9b81d54f
Ricardo Jericevich <rickyjericevich@gmail.com>
2024-08-15 21:43:06 +0200

    Initial commit

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+export COMPOSE_EXPERIMENTAL_GIT_REMOTE=true # see # see https://www.docker.com/blog/scaling-docker-compose-up/
+
+{ # try
+    docker compose up -d
+} || { # catch
+    # this catch should only be triggered when the 'docker' command fails or is run in a WSL distro that isn't enabled in Docker Desktop
+    # even if the WSL distro is not enabled in Docker Desktop, you can still run docker commands by using the 'docker.exe' command but there are various disadvantages to this
+        echo
+	    echo "'docker' command failed."
+		echo 
+	    echo "If you are using WSL, make sure Docker Desktop for Windows is running and that this WSL distro is enabled in Docker Desktop."
+	    echo "Otherwise, install and run docker as pers this repo: https://github.com/rickyjericevich/docker-setup-on-linux"
+}
